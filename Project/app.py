43d23628e5cc32b61929bb24d9115fb8528e34e1
Napat29102544 <napat29102544@gmail.com>
2023-09-11 00:12:13 +0700

    Queue and Stack

--- Project/app.py
+++ Project/app.py
@@ -1,62 +1,105 @@
 def run_01():
-    class Graph:
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+    class Queue:
         def __init__(self):
-            self.vertices = {}
+            self.front = None
+            self.tail = None
 
-        def add_vertex(self, vertex):
-            if vertex not in self.vertices:
-                self.vertices[vertex] = {}
+        def is_empty(self):
+            if self.front is None:
+                return None
 
-        def add_edge(self, vertex1, vertex2, weight):
-            if vertex1 in self.vertices and vertex2 in self.vertices:
-                self.vertices[vertex1][vertex2] = weight
-                self.vertices[vertex2][vertex1] = weight  # For an undirected graph
+        def enqueue(self, data):
+            new_node = Node(data)
+            if self.tail is None:
+                self.front = new_node
+                self.tail = new_node
+                return
+            self.tail.next = new_node
+            self.tail = new_node
+
+        def dequeue(self):
+            if self.is_empty():
+                print("Queue is empty")
+            dequeued_data = self.front.data
+            self.front = self.front.next
+            if self.front is None:
+                self.tail = None
+            return dequeued_data
+
+        def check(self):
+            if self.is_empty():
+                print("Queue is empty")
+            return self.front.data
 
         def display(self):
-            for vertex, neighbors in self.vertices.items():
-                for neighbor, weight in neighbors.items():
-                    print(f"{vertex},{neighbor},{weight}")
+            current = self.front
+            while current:
+                print(current.data)
+                current = current.next
+            print("No more queue")
 
-    def run01():
-        # Create a graph instance
-        the_graph = Graph()
-        # Vertex
-        the_graph.add_vertex('A')
-        the_graph.add_vertex('B')
-        the_graph.add_vertex('C')
-        the_graph.add_vertex('D')
-        # Edges
-        the_graph.add_edge('A', 'B', 3)
-        the_graph.add_edge('B', 'C', 5)
-        the_graph.add_edge('C', 'D', 7)
-        the_graph.add_edge('D', 'A', 9)
+    def run():
+        queue = Queue()
+        queue.enqueue(5)
+        queue.enqueue(10)
+        queue.enqueue(15)
+        queue.enqueue(20)
+        queue.display()
+        queue.dequeue()
+        queue.dequeue()
+        queue.display()
 
-        the_graph.display()
-
-    print(run01())
+    print(run())
 
 
 def run_02():
-    n = 3                                                                               # there are three nodes in example graph (graph is 1-based)
-    dist = [[0, 5, 10, 25], [10, 0, 5, 20], [15, 20, 0, 10], [30, 15, 25, 0]]           # Distance between city
-    memo = [[-1] * (1 << (n + 1)) for _ in range(n + 1)]                                # memoization for top down recursion
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
 
-    def fun(i, city):                                                                   # sub-problem
-        if city == ((1 << i) | 3):
-            return dist[1][i]                                                           # it implies we have visited all other nodes already
+    class Stack:
+        def __init__(self):
+            self.top = None
 
-        if memo[i][city] != -1:                                                         # memoization
-            return memo[i][city]
-        result = 99                                                                     # result of this sub-problem
+        def is_empty(self):
+            return self.top is None
 
-        for j in range(1, n + 1):                                                       # we have to travel all nodes j in mask and end the path at last node
-            if (city & (1 << j)) != 0 and j != i and j != 1:                            # travelling all nodes in mask except i and then travel back from node j to node i taking
-                result = min(result, fun(j, city & (~(1 << i))) + dist[j][i])           # the shortest path take the minimum of all possible j nodes
-        memo[i][city] = result                                                          # storing the minimum value
-        return result
+        def push(self, data):
+            new_node = Node(data)
+            new_node.next = self.top
+            self.top = new_node
 
-    ans = 99
-    for i in range(1, n + 1):                                                           # try to go from node 1 visiting all nodes in between to i
-        ans = min(ans, fun(i, (1 << (n + 1)) - 1) + dist[i][1])                         # return from i taking the shortest route to 1
+        def pop(self):
+            if self.is_empty():
+                return ("Stack is empty")
+            popped_data = self.top.data
+            self.top = self.top.next
+            return popped_data
 
-    print("The cost of most efficient tour = " + str(ans))
+        def check(self):
+            if self.is_empty():
+                print("Stack is empty")
+            return self.top.data
+
+        def display(self):
+            current = self.top
+            while current:
+                print(current.data)
+                current = current.next
+            print("No more stack")
+
+    def run():
+        stack = Stack()
+        stack.push(5)
+        stack.push(10)
+        stack.push(15)
+        stack.push(20)
+        stack.display()
+
+    print(run())
